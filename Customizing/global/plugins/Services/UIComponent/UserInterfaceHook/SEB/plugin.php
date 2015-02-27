0f45a57b7c2a759c70f79a5a66eee263da462ea3
Stefan Schneider <schneider@hrz.uni-marburg.de>
2015-02-27 10:06:29 +0100

    setting version range 5.0.x in plugin.php

--- Customizing/global/plugins/Services/UIComponent/UserInterfaceHook/SEB/plugin.php
+++ Customizing/global/plugins/Services/UIComponent/UserInterfaceHook/SEB/plugin.php
@@ -8,8 +8,8 @@ $version = "2.0.0";
 
 // ilias min and max version; must always reflect the versions that should
 // run with the plugin
-$ilias_min_version = "4.0.0";
-$ilias_max_version = "5.1.999";
+$ilias_min_version = "5.0.0";
+$ilias_max_version = "5.0.999";
 
 // optional, but useful: Add one or more responsible persons and a contact email
 $responsible = "Stefan Schneider";

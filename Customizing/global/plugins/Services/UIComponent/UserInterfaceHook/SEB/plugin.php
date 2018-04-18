72e2aa15f3bb16281fc29189e612b3e2b0f96b14
Stefan Schneider <schneider@hrz.uni-marburg.de>
2018-04-18 12:36:11 +0200

    initial 5.3 support

--- Customizing/global/plugins/Services/UIComponent/UserInterfaceHook/SEB/plugin.php
+++ Customizing/global/plugins/Services/UIComponent/UserInterfaceHook/SEB/plugin.php
@@ -9,7 +9,7 @@ $version = "2.3.1";
 // ilias min and max version; must always reflect the versions that should
 // run with the plugin
 $ilias_min_version = "5.0.0";
-$ilias_max_version = "5.2.999";
+$ilias_max_version = "5.3.999";
 
 // optional, but useful: Add one or more responsible persons and a contact email
 $responsible = "Stefan Schneider";
